af8a5a3e8275fcd56d147ca3055f8174aaea7854
JosueFabian <alexander.rivas2014@outlook.com>
2022-03-12 22:09:29 -0600

    Agregando activity para E2

---
+++ app/src/main/java/sv/edu/udb/loginparcial/Ejercicio2.java
@@ -0,0 +1,24 @@
+package sv.edu.udb.loginparcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Ejercicio2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio2);
+    }
+
+    public void calcular(View view) {
+        EditText votos = findViewById(R.id.etvotos);
+        
+
+    }
+
+
+    }
